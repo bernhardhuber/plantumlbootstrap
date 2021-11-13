f737d5a8aed5bb2f98d4968bdf9cdac218260a67
Bernhard Huber <bernihuber123@gmail.com>
2021-11-13 09:50:31 +0100

    add maven-invoker for integration-test with resteasy client

---
+++ src/main/it/resteasyclient/prebuild.groovy
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2021 berni3.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+This script may be written with either BeanShell or Groovy (since 1.3). 
+If the file extension is omitted (e.g. verify), the plugin searches for the 
+file by trying out the well-known extensions .bsh and .groovy. 
+
+If this script exists for a particular project but returns any non-null value 
+different from true or throws an exception, 
+the corresponding build is flagged as a failure.
+
+Default value is: prebuild.
+User property is: invoker.preBuildHookScript.
+*/
+
+def name='db-flyway'
+println "Prebuild $name!"
+
+return true
---
+++ src/main/it/resteasyclient/src/test/java/org/huberb/plantumlbootstrap/restesyclient/integrationtest/EncoderDecoderResourceIT.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2021 berni3.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.huberb.plantumlbootstrap.restesyclient.integrationtest;
+
+import java.util.function.Predicate;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+import org.jboss.resteasy.client.jaxrs.internal.ResteasyClientBuilderImpl;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author berni3
+ */
+public class EncoderDecoderResourceIT {
+
+    String urlBase = "http://localhost:8081/plantumlbootstrap-1.0-SNAPSHOT";
+
+    @Test
+    public void testEncode() {
+        final String url = urlBase + "/webresources/encoderDecoder/encode";
+        final String body = "@startuml\n"
+                + "Alice --> Bob : hello\n"
+                + "@enduml";
+
+        final ResteasyClient resteasyClient = new ResteasyClientBuilderImpl()
+                .build();
+        try (ResteasyClientAutoCloseable rcac = new ResteasyClientAutoCloseable(resteasyClient)) {
+            final ResteasyWebTarget resteasyWebTarget = resteasyClient.target(UriBuilder.fromPath(url));
+            // POST
+            try (final Response encodePostResponse = resteasyWebTarget
+                    .request()
+                    .accept(MediaType.APPLICATION_JSON)
+                    .post(Entity.entity(body, MediaType.TEXT_PLAIN))) {
+                final String entityRead = encodePostResponse.readEntity(String.class);
+                System.out.printf("testEncode:%n"
+                        + "HTTP code: %d%n"
+                        + "HTTP content-length: %d%n"
+                        + "has entity: %s%n"
+                        + "entity1: %s%n"
+                        + "entity2: %s%n",
+                        encodePostResponse.getStatus(),
+                        encodePostResponse.getLength(),
+                        "?X?", //encodePostResponse.hasEntity(),
+                        "?X?", //encodePostResponse.getEntity(),
+                        entityRead
+                );
+                final String m = "" + encodePostResponse;
+
+                assertEquals(Status.OK.getStatusCode(), encodePostResponse.getStatus(), m);
+
+                assertTrue(
+                        entityRead.contains("\"decoded\":")
+                        && entityRead.contains("\"encoded\":"),
+                        m);
+
+                final Predicate<String> pred1 = (s)
+                        -> s.contains("\"decoded\":")
+                        && s.contains("\"encoded\":");
+                assertTrue(pred1.test(entityRead), m);
+            }
+        }
+        assertTrue(resteasyClient.isClosed());
+    }
+
+    @Test
+    public void testDecode() {
+        final String url = urlBase + "/webresources/encoderDecoder/decode";
+        final String body = "AyaioKbL24ujB4tDImOoyZ8B2b9B50ovdFAJ57Jj51npCe72LWePgJav-L0U5uG2oe8KmUH0R000";
+
+        final ResteasyClient resteasyClient = new ResteasyClientBuilderImpl()
+                .build();
+        try (ResteasyClientAutoCloseable rcac = new ResteasyClientAutoCloseable(resteasyClient)) {
+            final ResteasyWebTarget resteasyWebTarget = resteasyClient.target(UriBuilder.fromPath(url));
+            // POST
+            try (final Response decodePostResponse = resteasyWebTarget
+                    .request()
+                    .accept(MediaType.APPLICATION_JSON)
+                    .post(Entity.entity(body, MediaType.TEXT_PLAIN))) {
+                final String entityRead = decodePostResponse.readEntity(String.class);
+                System.out.printf("testDecode:%n"
+                        + "HTTP code: %d%n"
+                        + "HTTP content-length: %d%n"
+                        + "has entity: %s%n"
+                        + "entity1: %s%n"
+                        + "entity2: %s%n",
+                        decodePostResponse.getStatus(),
+                        decodePostResponse.getLength(),
+                        "?X?", //encodePostResponse.hasEntity(),
+                        "?X?", //encodePostResponse.getEntity(),
+                        entityRead
+                );
+                final String m = "" + decodePostResponse;
+                assertEquals(Status.OK.getStatusCode(), decodePostResponse.getStatus(), m);
+
+                assertTrue(
+                        entityRead.contains("\"decoded\":")
+                        && entityRead.contains("\"encoded\":"),
+                        m);
+
+                final Predicate<String> pred1 = (s)
+                        -> s.contains("\"decoded\":")
+                        && s.contains("\"encoded\":");
+                assertTrue(pred1.test(entityRead), m);
+            }
+        }
+        assertTrue(resteasyClient.isClosed());
+    }
+
+    static class ResteasyClientAutoCloseable implements AutoCloseable {
+
+        private final ResteasyClient client;
+
+        public ResteasyClientAutoCloseable(ResteasyClient client) {
+            this.client = client;
+        }
+
+        public ResteasyClient client() {
+            return this.client;
+        }
+
+        @Override
+        public void close() {
+            if (client != null && !client.isClosed()) {
+                client.close();
+            }
+        }
+    }
+}
